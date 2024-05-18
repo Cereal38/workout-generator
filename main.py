b09c45b8506c4ba0465f0b5d1b9971fc517c50c1
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-18 12:24:49 +0200

    Create a shorten version of exercises.json

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 
 import sys
 
+from utils.json_extract import json_extract
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         sentence = sys.argv[1]
     else:
         sys.exit("Please provide a sentence as an argument.")
+
+    # Extract the exercises from the JSON file
+    exercises: list[dict[str, str]] = json_extract("exercises_simple.json")
