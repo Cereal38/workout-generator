436e8bac3a8118df8e4c9066e47b0cc8d8506e07
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-18 15:39:56 +0200

    Add an int id to every exercice

--- utils/json_extract.py
+++ utils/json_extract.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 def json_extract(input_file: str, no_equipment: bool = False) -> list[dict[str, str]]:
-    """Extracts the name and equipment of each exercise from a JSON file.
+    """Extracts the name and if of each exercise from a JSON file.
 
     Args:
       input_file (str): The path to the initial JSON file.
@@ -17,15 +17,18 @@ def json_extract(input_file: str, no_equipment: bool = False) -> list[dict[str, 
     with Path(input_file).open() as f:
         data = json.load(f)
 
-    # If equipment is False, exclude exercises that require equipment and remove the equipment field
+    # If no_equipment is True, exclude exercises that require equipment
     if no_equipment:
         return [
-            {"name": exercise["name"]}
+            {"name": exercise["name"], "id": exercise["id"]}
             for exercise in data
             if exercise["equipment"] is None or exercise["equipment"] == "body only"
         ]
 
     return [
-        {"name": exercise["name"], "equipment": exercise["equipment"]}
+        {
+            "name": exercise["name"],
+            "id": exercise["id"],
+        }
         for exercise in data
     ]
